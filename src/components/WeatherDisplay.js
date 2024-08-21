d41177cc581b5c4fff68fbe03edc2d96b0e4c671
Aisha Imtiyaz <144771688+aishaimtiyaz@users.noreply.github.com>
2024-08-21 08:32:24 +0000

    neww

--- src/components/WeatherDisplay.js
+++ src/components/WeatherDisplay.js
@@ -8,7 +8,7 @@ const WeatherDisplay = ({weatherInput}) => {
 
     <div>
          <h2>Current Weather</h2>
-         {weatherInput.temperature > 20 ? <span style={{color:"red"}}>Temperature: {weatherInput.temperature}</span> : <span style={{color:"blue"}}>Temperature: {weatherInput.temperature}</span>}
+        <p>Temperature: <span style={{color: weatherInput.temperature > 20 ? 'red' : 'blue'}}>{weatherInput.temperature} </span> </p>
         <p>Conditions: {weatherInput.conditions}</p>
     </div>
   )
